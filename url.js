1db75295eea8d7d6131b29e7dd3aac4c528d6003
Jack May <jack@solana.com>
2020-04-29 16:08:44 -0700

    Improve cluster selection (#24)

--- url.js
+++ url.js
@@ -2,28 +2,41 @@
 // environment. By default, `LIVE=1` will connect to the devnet cluster.
 
 import {clusterApiUrl, Cluster} from '@solana/web3.js';
+import fs from 'mz/fs';
 
 function chooseCluster(): Cluster | undefined {
-  if (!process.env.LIVE) return;
-  switch (process.env.CLUSTER) {
-    case 'devnet':
-    case 'testnet':
-    case 'mainnet-beta': {
-      return process.env.CLUSTER;
+  let cluster;
+  if (process.env.LIVE) {
+    cluster = process.env.CLUSTER;
+  } else if (fs.existsSync('cluster.txt')) {
+    cluster = fs
+      .readFileSync('cluster.txt')
+      .toString()
+      .replace(/(\r\n|\n|\r| )/gm, '');
+  }
+
+  if (cluster) {
+    switch (cluster) {
+      case 'devnet':
+      case 'testnet':
+      case 'mainnet-beta': {
+        return cluster;
+      }
+      default:
+        throw 'Unknown cluster: ' + cluster;
     }
   }
-  return 'devnet';
 }
 
 export const cluster = chooseCluster();
 
 export const url =
   process.env.RPC_URL ||
-  (process.env.LIVE ? clusterApiUrl(cluster, false) : 'http://localhost:8899');
+  (cluster ? clusterApiUrl(cluster, false) : 'http://localhost:8899');
 
 export const urlTls =
   process.env.RPC_URL ||
-  (process.env.LIVE ? clusterApiUrl(cluster, true) : 'http://localhost:8899');
+  (cluster ? clusterApiUrl(cluster, true) : 'http://localhost:8899');
 
 export let walletUrl =
   process.env.WALLET_URL || 'https://solana-example-webwallet.herokuapp.com/';
