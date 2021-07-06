71e5af2944c149c746c0256bc663f375242378c1
zurgl <elayar.yacine@gmail.com>
2021-07-06 02:35:00 +0200

    run prettier to pass CI

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -72,7 +72,9 @@ export async function getPayer(): Promise<Keypair> {
 /**
  * Create a Keypair from a secret key stored in file as bytes' array
  */
-export async function createKeypairFromFile(filePath: string): Promise<Keypair> {
+export async function createKeypairFromFile(
+  filePath: string,
+): Promise<Keypair> {
   const secretKeyString = await fs.readFile(filePath, {encoding: 'utf8'});
   const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
   return Keypair.fromSecretKey(secretKey);
