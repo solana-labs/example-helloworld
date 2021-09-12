7102d3e9fe431b047711af06533485495beaaa52
Justin Starry <justin@solana.com>
2021-09-11 19:09:24 -0500

    npm run lint:fix

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -6,7 +6,7 @@ import os from 'os';
 import fs from 'mz/fs';
 import path from 'path';
 import yaml from 'yaml';
-import {Keypair, Connection} from '@solana/web3.js';
+import {Keypair} from '@solana/web3.js';
 
 /**
  * @private
