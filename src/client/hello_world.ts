fa4cc296adda046500f620d4e6c65b6c5d9ab0c2
Justin Starry <justin@solana.com>
2021-09-11 19:09:24 -0500

    npm run lint:fix

--- src/client/hello_world.ts
+++ src/client/hello_world.ts
@@ -15,11 +15,7 @@ import fs from 'mz/fs';
 import path from 'path';
 import * as borsh from 'borsh';
 
-import {
-  getPayer,
-  getRpcUrl,
-  createKeypairFromFile,
-} from './utils';
+import {getPayer, getRpcUrl, createKeypairFromFile} from './utils';
 
 /**
  * Connection to the network
